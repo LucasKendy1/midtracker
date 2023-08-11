40c8d5a3c2f26ea47256617faada1ac02e06d912
LucasKendy1 <87815996+LucasKendy1@users.noreply.github.com>
2023-08-11 13:30:46 -0300

    a

--- src/App.js
+++ src/App.js
@@ -12,8 +12,28 @@ import New from './components/pages/New';
 import {useState} from 'react';
 import styles from './App.module.css'
 import Midia from './components/pages/Midia';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-function App() {
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAW4CFaE3y2gez7jzi2af3tHnv2nHOTwQQ",
+  authDomain: "midtracker-e799f.firebaseapp.com",
+  projectId: "midtracker-e799f",
+  storageBucket: "midtracker-e799f.appspot.com",
+  messagingSenderId: "679662573379",
+  appId: "1:679662573379:web:ccff1f08ade4993963c1f6",
+  measurementId: "G-XNG3T2T9NK"
+};
+
+// Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  function App() {
   const [menu, setMenu] = useState(false)
 
   function abrirHeader(){
